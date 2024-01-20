80d6b8faac1aee18fa94804ccb6bc5a64054ca71
Amogh Pathak <amogh9792@gmail.com>
2024-01-20 17:06:20 +0530

    solved errors

--- server/server.py
+++ server/server.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify
 import util
+
 app = Flask(__name__)
 
-@app.route('/get_location_name')
-def get_location_name():
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
     response = jsonify({
-        'locations' : util.get_location_names()
+        'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-@app.route('/predict_house_price', methods = ['GET', 'POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -19,12 +20,13 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimatedprice' : util.get_estimated_price(location,total_sqft, bhk, bath)
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
-
-    response.headers.add('Access-Control-Allow_Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-print("Starting python flask server for home price prediction")
-app.run()
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
